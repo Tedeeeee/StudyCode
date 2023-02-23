a8b9dcb3221d78b61c4dbef53d99716cc9a83c09
Tedeeeee <gksmf35916@gmail.com>
2023-02-23 23:04:12 +0900

    신입 사원

--- Backjoon_1946.java
+++ Backjoon_1946.java
@@ -55,4 +55,4 @@ class Grade implements Comparable<Grade> {
 }
 // 비교 할 수 있는 인자를 두개로 두는데 Comparable로 진행하여 0번째 인덱스(자신)과 1번째 인덱스를 비교한다
 // Grade 클래스를 만들어 interface Comparable의 메서드 compareTo를 사용하여 비교를 하였고
-// main클래스에서 list를 사용하여 불러왔다. 제네릭은 역시 grade로 받았고 나머지는 식의 계산이다.
+// main클래스에서 list를 사용하여 불러왔다. 제네릭은 역시 grade로 받았고 나머지는 식의 계산이다
